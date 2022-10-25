ee59c24c6ae78d2cd8d6fdf355cbd89158f3484d
kajikawa256 <kajikawa2003@gmail.com>
2022-10-25 14:13:16 +0900

    phpファイル

---
+++ Until.php
@@ -0,0 +1,49 @@
+<?php
+class Until{
+  public static $db;
+  public static $max;
+  public static $data;
+  public static $quesnum = 0;
+
+  function __construct(){
+    try {
+      self::$db = new PDO('mysql:dbname=enword;host=localhost;charset=utf8mb4', 'root', 'root');
+      // echo "接続OK！" . "\n";
+    } catch (PDOException $e) {
+      echo 'DB接続エラー！: ' . $e->getMessage();
+    }
+    //table名
+    self::$data = 'enword_table';
+    $data = self::$data;
+    // 対象テーブルを選択しSELECT文を変数tableへ格納
+    $table = "SELECT word,mean FROM $data";
+    // queryを実行し、結果を変数に格納
+    $db = self::$db;
+    $sql = $db->query($table);
+    self::$max = $sql->rowCount(); //レコード数を取得
+  }
+
+  // echo $max;
+  public function createMon(){
+    $index = array(0, 0, 0, 0);
+    foreach ($index as &$recod) {
+        // echo ":";
+        $recod = mt_rand(1, self::$max);
+    }
+    unset($recod);
+    echo '<br>';
+    $data = self::$data;
+    // ランダムな４つのレコードを取得するSQL
+    $makequest = "SELECT mean FROM $data WHERE id IN($index[0],$index[1],$index[2],$index[3])";
+    //SQL実行
+    $db = self::$db;
+    $quest4 = $db->query($makequest);
+    // 実行結果表示
+    // foreach ($quest4 as $row) {
+    //     echo $row['mean'] . '<br>';
+    // }
+    return $quest4;
+  }
+}
+
+?>
---
+++ questionpage.php
@@ -0,0 +1,77 @@
+<?php
+include('Until.php');
+$counter = 0;
+$Mon = new Until();
+$quest4 = $Mon -> createMon();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>英単語道場</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- ヘッダー部 -->
+        <header id="headerWrap" class="centeringBox">
+            <p id="logo">英</p>
+            <div id="header" class="centeringContent">
+                <a id="title" href="index.php">
+                    <h1>英<span>単</span>語道場</h1>
+                </a>
+            </div>
+        </header>
+        <!-- ヘッダー部終わり -->
+
+        <!-- メイン部 -->
+        <div id="contentWrap">
+            <div id="maincontent">
+                <div id="status">
+                    <div id="question_number">
+                        <p>N問目</p>
+                    </div>
+                    <div id="answer_rate">
+                        <p>正解数:Y回/出題数:N回&#009;<span>正答率--%</span></p>
+                    </div>
+                </div>
+                <div id="question_word">
+                    <h3>問題単語</h3>
+                </div>
+                <form id="form">
+                    <div id="answer_field">
+                        <ul id="answer_ul">
+                        <?php foreach($quest4 as $row): ?>
+                            <li>
+                                <button class="ansbutton"><a href="anspage.php"><?= ++$counter?></a></button><br>
+                                <p><?= $row['mean'] ?></p>
+                            </li>
+                        <?php endforeach; unset($row) ?>
+                        </ul>
+                    </div>
+                    <div id="nextquestion">
+                        <button id="font" type="submit"><a id="next" href="questionpage.php">次の問題へ</a></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- メイン部終わり -->
+
+        <!-- フッター部-->
+        <footer id="footerWrap">
+            <p>&copy; All rights reserved by webcampnavi.</p>
+            <div id="footerlogo">
+                <a href="index.php">英<span>単</span>語道場</a>
+            </div>
+        </footer>
+        <!-- フッター部終わり -->
+    </div>
+</body>
+
+</html>
